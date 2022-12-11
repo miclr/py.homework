c1a833ed98987541f5ff00664bd45e9d0d4e29fd
Your Name <you@example.com>
2022-12-11 10:16:51 +0330

    add aval

---
+++ add aval.py
@@ -0,0 +1,11 @@
+number = int (input ("enter number"))
+for this_num in range (2,number+1)
+  is_prime= true
+  for i in range (2, this_num):
+      break
+if is_prime:
+    print(this_num)
+    
+    
+    
+              
